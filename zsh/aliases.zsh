092857dc735750eced87ac0bc1b287699157b521
Mark Myers <marcusmyers@gmail.com>
2015-12-23 22:01:27 -0500

    Add aliases etc.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,3 +5,7 @@ function e_header()   { echo -e "\n\033[1m$@\033[0m"; }
 function e_success()  { echo -e " \033[1;32m✔\033[0m  $@"; }
 function e_error()    { echo -e " \033[1;31m✖\033[0m  $@"; }
 function e_arrow()    { echo -e " \033[1;34m➜\033[0m  $@"; }
+
+alias homestead='cd ~/Homestead && vagrant'
+alias dclean='docker rm `docker ps -a -q -f status=exited`'
+alias diclean='docker rmi `docker images -q -f dangling=true`'
