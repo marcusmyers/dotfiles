0978448a7e1a54df1df8090a86cd1ba686be6e58
Mark Myers <marcusmyers@gmail.com>
2016-11-16 22:32:07 -0500

    Reorganize aliases
    This commit reorganizes all the aliases by category in the zsh
    directory.

---
+++ zsh/php/aliases.zsh
@@ -0,0 +1,1 @@
+alias homestead='cd ~/Homestead && vagrant'
---
+++ zsh/shell/aliases.zsh
@@ -0,0 +1,20 @@
+# Shell commmands
+alias c='clear'
+alias e='exit'
+alias tf='tail -f'
+
+# Environment Reloading
+reload_shell() {
+  source ~/.zshrc
+  if ! direnv status | grep -q "No .envrc found" ; then
+    direnv reload
+  fi
+}
+
+alias r='reload_shell'
+
+# Undo auto-correct
+alias lunchy='nocorrect lunchy'
+
+# Alias a default Siege command
+alias attack='siege -t20s -b -c1'
