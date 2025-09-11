f5d63f2b6a3ee1d1b2375d1a035fb9fe491e717a
marcusmyers <marcusmyers@gmail.com>
2025-09-11 15:09:24 -0400

    Start the process to moving to gnu stow for dotfiles

---
+++ pacman/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo pacman -S --noconfirm --needed \
+    bat \
+    kitty \
+    lolcat \
+    nodejs \
+    npm \
+    oh-my-posh-bin \
+    php \
+    php-sqlite \
+    stow \
+    tmux \
+    zsh
